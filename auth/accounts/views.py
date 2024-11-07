5051f076d1b74fc7867543d067a8f569e0543e1d
Reza Heydari <mrreza1394@gmail.com>
2024-11-07 16:05:29 +0330

    register users feature

--- auth/accounts/views.py
+++ auth/accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+User = get_user_model()
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
